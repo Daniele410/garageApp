ab7cd360221cfac24ebed8345fe4af7806774ccc
Daniele410 <miraglia.daniele410@gmail.com>
2023-08-01 12:20:52 +0200

    add link Ui to backend

--- garageUI/src/app/app.component.ts
+++ garageUI/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {GarageService} from "./garage.service";
+import {OnInit} from "@angular/core";
+import {Car} from "./model/Car";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[GarageService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'garageUI';
+  cars: Car[] = [];
+
+  constructor(private garageService:GarageService) {
+  }
+
+  ngOnInit() {
+    console.log('On init...')
+    this.garageService.getCars().subscribe((datas)=> {
+      this.cars = datas;
+    })
+  }
 }
+
+
+
+
+
+
---
+++ garageUI/src/app/garage.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Car} from "./model/Car";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GarageService {
+
+  readonly API_URL = "http://localhost:8080"
+
+  readonly ENDPOINT_CARS ="/cars"
+
+  constructor(private httpClient:HttpClient) { }
+
+
+  getCars(){
+    return this.httpClient.get<Car[]>(this.API_URL+this.ENDPOINT_CARS)
+  }
+}
